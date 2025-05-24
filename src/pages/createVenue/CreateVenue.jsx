191deace3260310d045a19faf140bf9f70cf8394
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-05-25 01:33:17 +0700

    deloyment errors, lodash?

--- src/pages/createVenue/CreateVenue.jsx
+++ src/pages/createVenue/CreateVenue.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import CreateVenueForm from "../../components/ui/CreateVenueForm";
 import { createVenue } from "../../api/CreateVenue";
 import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 import { useNavigate } from "react-router-dom";
 
 const CreateVenuePage = () => {
